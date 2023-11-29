d60dedaa2f7d541113886a14d5630509b415ca39
HTL <130932491+htluu-ucsd@users.noreply.github.com>
2023-11-28 16:50:33 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,10 +1,9 @@
 -e
 # CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm -rf student-submission
-rm -rf grading-area
-
-mkdir grading-area
+#rm -rf student-submission
+#rm -rf grading-area
+#mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
